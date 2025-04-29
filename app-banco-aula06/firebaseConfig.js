46492f569533e4f9704ff938fc2c5f80e45e6d13
Cynthia Maia <cynthiamaia45@gmail.com>
2025-04-29 19:01:00 -0300

    Removendo um codigo

--- app-banco-aula06/firebaseConfig.js
+++ app-banco-aula06/firebaseConfig.js
@@ -3,12 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG-_bIv5fYnSj2oHclTIPqxdXxU85BGR8",
-  authDomain: "dispositivos-moveis-terca.firebaseapp.com",
-  projectId: "dispositivos-moveis-terca",
-  storageBucket: "dispositivos-moveis-terca.firebasestorage.app",
-  messagingSenderId: "33760057247",
-  appId: "1:33760057247:web:99e5432dfdfae07c9b0faa"
 };
 
 const app = initializeApp(firebaseConfig);
